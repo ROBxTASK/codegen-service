9d12719fcb056ee6882f84a8d7ca249d0f645acb
ALIENvsROBOT <gowtham.sridher5@gmail.com>
2022-10-10 13:39:49 +0200

    updated indentation mistake in 'selection function' ros generator

--- server/source/codegen_generator_ros.py
+++ server/source/codegen_generator_ros.py
@@ -152,5 +152,4 @@ class ROSGeneratorClass():
 		self.c.write('print (\'----------------------------------\')\n')
 		self.c.write('print (\'Generated Selection\')\n')
 		self.c.write('print (\'----------------------------------\')\n')
-		self.c.write('if ' + slotValue + ':\n\n')
-		self.c.dedent()
+		self.c.write('if ' + slotValue + ':'+self.c.dedent()+'\n\n')
